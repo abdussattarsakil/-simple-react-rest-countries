7b992cf39fa3c46f85a58943be1c5708cfeda484
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-04-20 20:27:49 +0600

    use state add for visited , not visited btn

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
 const Country = ({ country }) => {
-    const { name, area, population, flags ,continents ,region} = country
-    return (
-        <div className='card'>
-            <div>
-                <h2 className='normal text-h1'>Name: {name.common}</h2>
-                <h3>{name.official}</h3>
-                <p>Continents :- {continents.continents}</p>
-                <p>Region:- {region.region}</p>
-                <p>Area:-{area.area} , Population:-{population.population}</p>
-            </div>
+    const { name, area, population, flags, continents, region } = country
 
-            <div>
-                <img src={flags.flags.png} alt={flags.flags.alt} />
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        console.log("success btn clicked")
+        setVisited(true);
+    }
+
+    const handleNotVisited = () => {
+        setVisited(false)
+    }
+
+    return (
+        <>
+            <div >
+                <div className='card'>
+                    <div>
+                        <h2 className='normal text-h1'>Name: {name.common}</h2>
+                        <h3>{name.official}</h3>
+                        <p>Continents :- {continents.continents}</p>
+                        <p>Region:- {region.region}</p>
+                        <p>Area:-{area.area} , Population:-{population.population}</p>
+                    </div>
+
+                    <div>
+                        <img src={flags.flags.png} alt={flags.flags.alt} />
+                    </div>
+
+                    <div className='btn'>
+                        <button onClick={handleVisited}>
+                            Visited ??
+                        </button>
+                        <button onClick={handleNotVisited}>
+                            Not Visited ??
+                        </button>
+
+                    </div>
+                    <button className='visited-info'>
+                        {
+                            visited ? <p className='visited'>Visited</p> : <p className='notVisited'> Not Visited</p>
+                        }
+                    </button>
+                </div>
+
             </div>
-        </div>
+        </>
     );
 };
 
